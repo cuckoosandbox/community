f3e08444c2f446832f97b4c02a488f039ec1e41d
Thomas Birn <thomas030584@gmail.com>
2012-10-01 17:38:25 +0200

    Added Signature createautoruninf.py
    Signature for detecting the creation of an autorun.inf file

---
+++ modules/signatures/createsautoruninf.py
@@ -0,0 +1,16 @@
+from lib.cuckoo.common.abstracts import Signature
+
+class CreatesAutorunInf(Signature):
+    name = "createsautoruninf"
+    description = "Creates an autorun.inf file"
+    severity = 2
+    categories = ["generic"]
+    authors = ["Thomas Birn"]
+    minimum = "0.4.1"
+
+        for file_name in results["behavior"]["summary"]["files"]:
+            if "autorun.inf" in file_name:
+                self.data.append({"file": file_name})
+                return True
+
+        return False
