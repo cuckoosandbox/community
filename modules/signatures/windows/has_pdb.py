18f9818fb1f8a01fcac344681e0e8749360fee8a
Jurriaan Bremer <jurriaanbremer@gmail.com>
2015-10-03 17:30:48 +0200

    has pdb signature

--- modules/signatures/windows/has_pdb.py
+++ modules/signatures/windows/has_pdb.py
@@ -4,11 +4,11 @@
 
 from lib.cuckoo.common.abstracts import Signature
 
-class HasAuthenticode(Signature):
-    name = "has_authenticode"
-    description = "This executable is signed"
+class HasPdb(Signature):
+    name = "has_pdb"
+    description = "This executable has a PDB path"
     severity = 1
 
     def on_complete(self):
-        if self.get_results("static", {}).get("signature"):
+        if self.get_results("static", {}).get("pdb_path"):
             return True
