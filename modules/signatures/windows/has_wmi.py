90c13979f67da5269c35303a61fa2aa3ef4c1899
Jurriaan Bremer <jbr@cuckoo.sh>
2016-03-07 20:55:06 +0100

    setup wmi blacklist to increase severity

--- modules/signatures/windows/has_wmi.py
+++ modules/signatures/windows/has_wmi.py
@@ -2,6 +2,8 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
+import re
+
 from lib.cuckoo.common.abstracts import Signature
 
 class HasWMI(Signature):
@@ -9,8 +11,13 @@ class HasWMI(Signature):
     description = "Executes one or more WMI queries"
     severity = 2
 
+    blacklist = "(AntivirusProduct|FirewallProduct)"
+
     def on_complete(self):
         for query in self.get_wmi_queries():
             self.mark_ioc("wmi", query)
+
+            if re.search(self.blacklist, query, re.I):
+                self.severity = 3
 
         return self.has_marks()
