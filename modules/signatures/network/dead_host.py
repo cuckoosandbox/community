c3f54a43c947fb09b002af4a62562a1fad825d90
justaguy <justaguy@justaguy.pw>
2016-04-08 18:43:34 +0200

    Adding a whitelist on the DeadHost signature.

--- modules/signatures/network/dead_host.py
+++ modules/signatures/network/dead_host.py
@@ -6,31 +6,33 @@ from lib.cuckoo.common.abstracts import Signature
 
 class DeadHost(Signature):
     name = "dead_host"
-    description = "Connects to an IP address that is no longer responding " \
-        "to requests (legitimate services will remain up-and-running usually)"
+    description = (
+        "Connects to an IP address that is no longer responding to "
+        "requests (legitimate services will remain up-and-running usually)"
+    )
     severity = 3
     categories = ["network"]
     authors = ["Cuckoo Technologies"]
     minimum = "2.0"
 
-    def on_complete(self):
-        # Don't run this signature for analyses that didn't have internet
-        # access at all.
-        if self.get_results("info", {}).get("route") == "none":
-            return
+    whitelist = [
+        "13.107.4.50",
+    ]
 
+    def on_complete(self):
         for ip, port in self.get_results("network", {}).get("dead_hosts", []):
-            self.mark_ioc("dead_host", "%s:%s" % (ip, port))
-            self.severity += 2
+            if ip not in self.whitelist:
+                self.mark_ioc("dead_host", "%s:%s" % (ip, port))
+                self.severity += 2
 
         if self.severity > 8:
             self.severity = 8
 
         if self.has_marks(2):
-            self.description = " ".join("""
-                Connects to IP addresses that are no longer responding to
-                requests (legitimate services will remain up-and-running
-                usually)
-            """.split())
+            self.description = (
+                "Connects to IP addresses that are no longer responding to "
+                "requests (legitimate services will remain up-and-running "
+                " usually)"
+            )
 
         return self.has_marks()
