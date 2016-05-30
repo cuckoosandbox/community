0d8029f3c7f7c78cf13f355aa75d142fbdd0db01
Jurriaan Bremer <jbr@cuckoo.sh>
2016-05-30 14:00:08 +0200

    sig cleanup

--- modules/signatures/windows/antianalysis_detectfile.py
+++ modules/signatures/windows/antianalysis_detectfile.py
@@ -1,10 +1,6 @@
 # Copyright (C) 2015 KillerInstinct, Updated 2016 for cuckoo 2.0
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
-try:
-    import re2 as re
-except ImportError:
-    import re
 
 from lib.cuckoo.common.abstracts import Signature
 
@@ -17,12 +13,12 @@ class AntiAnalysisDetectFile(Signature):
     minimum = "2.0"
 
     file_indicators = [
-         "^[A-Za-z]:\\\\analysis",
-         "^[A-Za-z]:\\\\iDEFENSE",
-         "^[A-Za-z]:\\\\popupkiller.exe$",
-         "^[A-Za-z]:\\\\tools\\\\execute.exe$",
-         "^[A-Za-z]:\\\\Program\\ Files(\\ \(x86\))?\\\\Fiddler",
-         "^[A-Za-z]:\\\\ComboFix",
+        "^[A-Za-z]:\\\\analysis",
+        "^[A-Za-z]:\\\\iDEFENSE",
+        "^[A-Za-z]:\\\\popupkiller.exe$",
+        "^[A-Za-z]:\\\\tools\\\\execute.exe$",
+        "^[A-Za-z]:\\\\Program\\ Files(\\ \(x86\))?\\\\Fiddler",
+        "^[A-Za-z]:\\\\ComboFix",
     ]
 
     def on_complete(self):
