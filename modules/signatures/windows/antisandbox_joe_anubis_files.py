ddac30e68e9d234fb2737fda9325242d4b294745
kevross33 <kevross33@gmail.com>
2016-06-17 11:50:29 +0100

    Remove trailing $ from anubis file sig

--- modules/signatures/windows/antisandbox_joe_anubis_files.py
+++ modules/signatures/windows/antisandbox_joe_anubis_files.py
@@ -24,7 +24,7 @@ class SandboxJoeAnubisDetectFiles(Signature):
     minimum = "2.0"
 
     file_indicators = [
-         "C\:\\\\sample\.exe$",
+         "C\:\\\\sample\.exe",
          "C\:\\\\InsideTm\\\\.*",
     ]
 
