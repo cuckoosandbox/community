310cdf3ae232c76769de12ae1955238020e95ea7
kevross33 <kevross33@gmail.com>
2016-07-13 10:13:44 +0100

    Update persistence_ads.py

--- modules/signatures/windows/persistence_ads.py
+++ modules/signatures/windows/persistence_ads.py
@@ -13,7 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 try:
     import re2 as re
 except ImportError:
