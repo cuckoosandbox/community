91ce7343d337699086331b652757c345954509f0
kevross33 <kevross33@gmail.com>
2016-08-01 12:32:36 +0100

    Create signature for malware checking for clipboard data (i.e for sandbox detection)

---
+++ modules/signatures/windows/antisandbox_clipboard.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2016 Kevin Ross
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from lib.cuckoo.common.abstracts import Signature
+
+class AntisandboxClipboard(Signature):
+    name = "antisandbox_clipboard"
+    description = "Checks contents of the clipboard which can potentially be used for sandbox detection"
+    severity = 2
+    categories = ["anti-sandbox"]
+    authors = ["Kevin Ross"]
+    minimum = "2.0"
+
+    filter_apinames = set(["GetClipboardData"])
+
+    def on_call(self, call, process):
+        self.mark_call()
+
+    def on_complete(self):
+        return self.has_marks()
