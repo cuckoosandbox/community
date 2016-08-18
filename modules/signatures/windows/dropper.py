4e3093126cf0fec8d645bf5b5013b9d8a8487202
kevross33 <kevross33@gmail.com>
2016-08-18 14:50:17 +0100

    Add in dropper signature
    Modified from the concept but this works it seems. Here is the original:
    
    https://github.com/spender-sandbox/community-modified/blob/master/modules/signatures/dropper.py

---
+++ modules/signatures/windows/dropper.py
@@ -0,0 +1,45 @@
+# Copyright (C) 2014 Optiv Inc. (brad.spengler@optiv.com), Updated 2016 for Cuckoo 2.0
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from lib.cuckoo.common.abstracts import Signature
+
+class Dropper(Signature):
+    name = "dropper"
+    description = "Drops a binary and executes it"
+    severity = 2
+    categories = ["dropper"]
+    authors = ["Optiv"]
+    minimum = "2.0"
+
+    def __init__(self, *args, **kwargs):
+        Signature.__init__(self, *args, **kwargs)
+        self.executed = []
+
+    filter_apinames = set(["CreateProcessInternalW","ShellExecuteExW"])
+
+    def on_call(self, call, process):
+        filepath = call["arguments"]["filepath"]
+        if filepath not in self.executed:
+            self.executed.append(filepath)
+
+    def on_complete(self):
+        for executed in self.executed:
+            for dropped in self.get_results("dropped", []):
+                if "filepath" in dropped:
+                    filepath = dropped["filepath"]
+                    if executed == filepath:
+                        self.mark_ioc("file", executed)
+
+        return self.has_marks()
