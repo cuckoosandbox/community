d7b27a1f6ec34ad64c43dc554b908798bd075213
Thorsten Sick <thorsten.sick@avira.com>
2016-09-23 14:51:53 +0200

    Adding Avira Scout browser to monitored browsers

--- modules/signatures/windows/infostealer_browser.py
+++ modules/signatures/windows/infostealer_browser.py
@@ -36,6 +36,7 @@ class BrowserStealer(Signature):
         ".*\\\\(Application\\ Data|AppData).*?\\\\Bromium\\\\.*",
         ".*\\\\(Application\\ Data|AppData).*?\\\\RockMelt\\\\.*",
         ".*\\\\(Application\\ Data|AppData).*?\\\\Yandex\\\\YandexBrowser\\\\.*",
+        ".*\\\\(Application\\ Data|AppData).*?\\\\Local\\\\Avira\\\\Scout\\\\User Data\\\\.*",
     ]
 
     regkeys_re = [
