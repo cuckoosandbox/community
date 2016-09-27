6e7783c61c473577ecbe0068c6b29254f7670def
kevross33 <kevross33@gmail.com>
2016-09-27 09:52:19 +0100

    Add in Locky odin variant file

--- modules/signatures/windows/ransomware_files.py
+++ modules/signatures/windows/ransomware_files.py
@@ -57,6 +57,7 @@ class RansomwareFiles(Signature):
         (".*\\\\HOW_TO_UNLOCK_FILES_README_\([0-9a-f]+\)\.(txt|html|bmp)$", ["WildFire"]),
         (".*\\\\HELP_DECRYPT_YOUR_FILES\.(html|txt)$", ["CryptFile2"]),
         (".*\\\\!!!\ Readme\ For\ Decrypt\ !!!\.txt$", ["MarsJoke"]),
+        (".*_HOWDO_text\.(html|bmp)$", ["Locky"]),        
     ]
 
     def on_complete(self):
