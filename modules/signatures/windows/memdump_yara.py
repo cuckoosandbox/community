f71a7ed72454c531a295d2f2cfd2a96b073924e9
kevross33 <kevross33@gmail.com>
2016-10-21 10:36:10 +0100

    Create rule for process memory dump yara hit
    Created a rule to detect yara signature hits for process memory. Never actually used the cuckoo-modified signature to create this but as one exists & as I may use concepts from it in the future I have credited author too as concept is same. modified signature here:
    
    https://github.com/spender-sandbox/community-modified/blob/master/modules/signatures/procmem_yara.py

---
+++ modules/signatures/windows/memdump_yara.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2016 Kevin Ross, KillerInstinct
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from lib.cuckoo.common.abstracts import Signature
+
+class ProcMemDumpYara(Signature):
+    name = "memdump_yara"
+    description = "Yara rule detection in process memory"
+    severity = 2
+    categories = ["generic"]
+    authors = ["Kevin Ross", "KillerInstinct"]
+    minimum = "2.0"
+
+    def on_complete(self):
+        for procmem in self.get_results("procmemory", []):
+            for yara in procmem.get("yara", []):
+                yararule = yara["name"]
+                ruledescription = yara["meta"]["description"]
+                self.mark(
+                    rule=yararule,
+                    description=ruledescription,                      
+                )
+
+        return self.has_marks()
