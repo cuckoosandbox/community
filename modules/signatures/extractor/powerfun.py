023d5465390866d08967ac8f5a004c2944dbaf16
Fernando <fernando@cuckoo.sh>
2017-05-08 13:37:44 +0200

    Extract the shellcode and not whole script

--- modules/signatures/extractor/powerfun.py
+++ modules/signatures/extractor/powerfun.py
@@ -10,9 +10,15 @@ class Powerfun(Extractor):
         argre = re.compile("FromBase64String\(['\"]([^)]+)['\"]\)")
         arg = argre.search(sc)
         if arg:
-            self.push_shellcode(
-                zlib.decompress(
-                    arg.group(1).replace("'", "").decode('base64'),
-                    16 + zlib.MAX_WBITS
-                )
+            # Powerfun invokes a second-stage PS script
+            # This script is b64encoded and gziped
+            script = zlib.decompress(
+                arg.group(1).replace("'", "").decode('base64'),
+                16 + zlib.MAX_WBITS
             )
+            # The shellcode in the script is also b64 encoded
+            arg = argre.search(script)
+            if arg:
+                self.push_shellcode(
+                    arg.group(1).replace("'", "").decode('base64')
+                )
