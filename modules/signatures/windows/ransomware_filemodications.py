33d4f721aca31f289291ab98cb49e91f8f8556ec
kevross33 <kevross33@gmail.com>
2017-05-24 23:30:23 +0100

    Make changes to the count to report better

--- modules/signatures/windows/ransomware_filemodications.py
+++ modules/signatures/windows/ransomware_filemodications.py
@@ -15,38 +15,39 @@ from lib.cuckoo.common.abstracts import Signature
 
 class RamsomwareFileMoves(Signature):
     name = "ransomware_file_moves"
-    description = "Performs more than %d file moves indicative of a ransomware file encryption process"
+    description = "Performs %d file moves indicative of a ransomware file encryption process"
     severity = 3
     categories = ["ransomware"]
     minimum = "2.0"
 
     filter_apinames = "MoveFileWithProgressW", "MoveFileWithProgressTransactedW"
+
+    count = 0
 
     def on_call(self, call, process):
         origfile = call["arguments"]["oldfilepath"]
         newfile = call["arguments"]["newfilepath"]
         if not origfile.endswith(".tmp") and not newfile.endswith(".tmp"):
+            self.count += 1
             self.mark_call()
 
     def on_complete(self):
-        if self.has_marks(1000):
-            self.description = self.description % 1000
-            self.severity = 6
-        elif self.has_marks(500):
-            self.description = self.description % 500
-            self.severity = 5
-        elif self.has_marks(100):
-            self.description = self.description % 100
-            self.severity = 4
-        elif self.has_marks(50):
-            self.description = self.description % 50
-            self.severity = 3
+        if self.count > 50:
+            self.description = self.description % self.count
+            if self.has_marks(1000):
+                self.severity = 6
+            elif self.has_marks(500):
+                self.severity = 5
+            elif self.has_marks(100):
+                self.severity = 4
+            elif self.has_marks(50):
+                self.severity = 3
 
-        return self.has_marks(50)
+            return self.has_marks()
 
 class RansomwareAppendsExtension(Signature):
     name = "ransomware_appends_extensions"
-    description = "Appends a new file extension to more than %d files indicative of a ransomware file encryption process"
+    description = "Appends a new file extension to %d files indicative of a ransomware file encryption process"
     severity = 3
     categories = ["ransomware"]
     authors = ["Kevin Ross"]
@@ -54,40 +55,41 @@ class RansomwareAppendsExtension(Signature):
 
     filter_apinames = "MoveFileWithProgressW", "MoveFileWithProgressTransactedW"
 
+    count = 0
+
     def on_call(self, call, process):
         origfile = call["arguments"]["oldfilepath"]
         newfile = call["arguments"]["newfilepath"]
         if origfile != newfile and not origfile.endswith(".tmp") and not newfile.endswith(".tmp"):
+            self.count += 1
             self.mark_call()
 
     def on_complete(self):
-        if self.has_marks(1000):
-            self.description = self.description % 1000
-            self.severity = 6
-        elif self.has_marks(500):
-            self.description = self.description % 500
-            self.severity = 5
-        elif self.has_marks(100):
-            self.description = self.description % 100
-            self.severity = 4
-        elif self.has_marks(50):
-            self.description = self.description % 50
-            self.severity = 3
+        if self.count > 50:
+            self.description = self.description % self.count
+            if self.has_marks(1000):
+                self.severity = 6
+            elif self.has_marks(500):
+                self.severity = 5
+            elif self.has_marks(100):
+                self.severity = 4
+            elif self.has_marks(50):
+                self.severity = 3
 
-        return self.has_marks(50)
+            return self.has_marks()
 
 from lib.cuckoo.common.abstracts import Signature
 
 class RansomwareDroppedFiles(Signature):
     name = "ransomware_dropped_files"
-    description = "Drops many unknown file mime types indicative of ransomware writing encrypted files back to disk"
+    description = "Drops %d unknown file mime types indicative of ransomware writing encrypted files back to disk"
     severity = 3
     categories = ["ransomware"]
     authors = ["Kevin Ross"]
     minimum = "2.0"
 
     def on_complete(self):
-        droppedcount = 0
+        count = 0
 
         for dropped in self.get_results("dropped", []):
             if "filepath" in dropped:
@@ -95,13 +97,14 @@ class RansomwareDroppedFiles(Signature):
                 droppedname = dropped["name"]
                 filepath = dropped["filepath"]
                 if droppedtype == "data" and ".tmp" not in droppedname:
-                    droppedcount += 1
+                    count += 1
                     self.mark_ioc("file", filepath)
-        if droppedcount > 50:
-            if droppedcount > 1000:
+        if count > 50:
+            self.description = self.description % count
+            if count > 1000:
                 self.severity = 6
-            elif droppedcount > 500:
+            elif count > 500:
                 self.severity = 5
-            elif droppedcount > 200:
+            elif count > 200:
                 self.severity = 4
             return self.has_marks()
