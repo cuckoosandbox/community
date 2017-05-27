910cebdcae0da1dfaa371cae1bb5434eb0335a84
Jurriaan Bremer <jbr@cuckoo.sh>
2017-05-27 14:58:53 +0200

    signature cleanup

--- modules/signatures/windows/exec_bitsadmin.py
+++ modules/signatures/windows/exec_bitsadmin.py
@@ -1,18 +1,10 @@
-# Copyright (C) 2010-2015 Cuckoo Foundation.
+# Copyright (C) 2017 Cuckoo Foundation.
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
-import shlex
-import yara
-import logging
-import traceback
 import re
 
 from lib.cuckoo.common.abstracts import Signature
-from cuckoo.common.objects import URL_REGEX
-from cuckoo.misc import cwd
-
-log = logging.getLogger()
 
 class ExecBitsAdmin(Signature):
     name = "exec_bits_admin"
@@ -25,9 +17,9 @@ class ExecBitsAdmin(Signature):
     def on_complete(self):
         lower = "".join(self.get_command_lines()).lower()
         if "bitsadmin" in lower and "/download" in lower:
-            cmdre = re.compile("bitsadmin .+ \/download .* (http:\/\/[^\s]+)")
-            url = cmdre.search(lower)
+            url = re.search(
+                "bitsadmin .+ \/download .* (http:\/\/[^\s]+)", lower
+            )
             if url:
                 self.mark_ioc("url", url.group(1))
             return True
-
--- modules/signatures/windows/exec_waitfor.py
+++ modules/signatures/windows/exec_waitfor.py
@@ -1,18 +1,10 @@
-# Copyright (C) 2010-2015 Cuckoo Foundation.
+# Copyright (C) 2017 Cuckoo Foundation.
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
-import shlex
-import yara
-import logging
-import traceback
 import re
 
 from lib.cuckoo.common.abstracts import Signature
-from cuckoo.common.objects import URL_REGEX
-from cuckoo.misc import cwd
-
-log = logging.getLogger()
 
 class ExecWaitFor(Signature):
     name = "exec_waitfor"
@@ -25,9 +17,7 @@ class ExecWaitFor(Signature):
     def on_complete(self):
         lower = "".join(self.get_command_lines()).lower()
         if "waitfor" in lower:
-            cmdre = re.compile("waitfor \/t \d+")
-            cmd = cmdre.search(lower)
+            cmd = re.search("waitfor \/t \d+", lower)
             if cmd:
                 self.mark_ioc("cmd", cmd.group(0))
             return True
-
