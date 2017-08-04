8fe07a7247e77eeaa06b2820752744b53a6c1116
kevross33 <kevross33@gmail.com>
2017-08-04 09:30:04 +0100

    Create sig for writing EXE to memory of another process
    Seen in Dridex MD5 2eaf243bad4b1c22089e7654524f0e5a

---
+++ modules/signatures/windows/injection_writemem_exe.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2017 Kevin Ross
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from lib.cuckoo.common.abstracts import Signature
+
+class InjectionWriteMemoryEXE(Signature):
+    name = "injection_write_memory_exe"
+    description = "Writes an executable/dll to the memory of another process"
+    severity = 3
+    categories = ["injection", "unpacking"]
+    authors = ["Kevin Ross"]
+    minimum = "2.0"
+
+    filter_apinames = [
+        "WriteProcessMemory",
+    ]
+
+    def on_call(self, call, process):
+        if call["api"] == "WriteProcessMemory":
+            if call["arguments"]["buffer"].startswith("MZ") and not call["arguments"]["process_handle"].startswith("0xfffffff"):
+                self.mark_call()
+
+    def on_complete(self):
+        return self.has_marks()
