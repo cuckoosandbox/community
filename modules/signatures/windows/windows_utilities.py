77534d8d3ce05511a3327e768e5f202974326ad2
kevross33 <kevross33@gmail.com>
2017-08-24 09:36:08 +0100

    Update windows_utilities.py

--- modules/signatures/windows/windows_utilities.py
+++ modules/signatures/windows/windows_utilities.py
@@ -4,6 +4,145 @@
 
 from lib.cuckoo.common.abstracts import Signature
 
+utilities = [
+    "at ",
+    "at.exe",
+    "attrib",
+    "chcp",
+    "del ",
+    "del.exe",
+    "dir ",
+    "dir.exe",
+    "driverquery",
+    "erase ",
+    "erase.exe",
+    "fsutil",
+    "getmac",
+    "ipconfig",
+    "nbtstat",
+    "net ",
+    "net.exe",
+    "netsh",
+    "netstat",
+    "nslookup",
+    "pathping",
+    "ping ",
+    "ping.exe",
+    "qwinsta",
+    "reg ",
+    "reg.exe",
+    "regsrv32",
+    "route",
+    "runas",
+    "rwinsta",
+    "sc ",
+    "sc.exe",
+    "schtasks",
+    "shutdown",
+    "sigverif",
+    "systeminfo",
+    "tasklist",
+    "taskkill",
+    "telnet",
+    "whoami",
+    "wmic",
+    "wusa",
+]
+
+risk_utilities = [
+    "bitsadmin",
+    "cacls",
+    "csvde",
+    "dsquery",
+    "icacls",
+    "nltest",
+    "rexec",
+    "sdbinst",
+    "volumeid",
+    "vssadmin",
+    "wevtutil",
+    "whois",
+    "xcacls",
+]
+
+sysinternals = [
+    "accesschk",
+    "accessenum",
+    "adexplorer",
+    "adinsight",
+    "adrestore",
+    "autologon",
+    "autoruns",
+    "bginfo",
+    "bluescreen",
+    "clockres",
+    "contig",
+    "coreinfo",
+    "ctrl2cap",
+    "debugview",
+    "desktops",
+    "disk2vhd",
+    "diskext",
+    "diskmon",
+    "du ",
+    "du.exe",
+    "efsdump",
+    "findlinks",
+    "handle ",
+    "handle.exe",
+    "hex2dec",
+    "junction",
+    "ldmdump",
+    "listdlls",
+    "livekd",
+    "loadorder",
+    "logonsessions",
+    "movefile",
+    "notmyfault",
+    "ntfsinfo",
+    "pendmoves",
+    "pipelist",
+    "portmon",
+    "procdump",
+    "psexec",
+    "psfile",
+    "bginfo",
+    "psgetsid",
+    "psinfo",
+    "pskill",
+    "pslist",
+    "psloggedon",
+    "psloglist",
+    "pspasswd",
+    "psping",
+    "psservice",
+    "psshutdown",
+    "pssuspend",
+    "pstools",
+    "rammap",
+    "regdelnull",
+    "ru ",
+    "ru.exe",
+    "regjump",
+    "sdelete",
+    "shareenum",
+    "shellrunas",
+    "sigcheck",
+    "streams ",
+    "streams.exe",
+    "strings ",
+    "strings.exe",
+    "sync ",
+    "sync.exe",
+    "sysmon",
+    "tcpview",
+    "vmmap",
+    "volumeid",
+    "whois",
+    "winobj",
+    "zoomit",
+]
+
 class UsesWindowsUtilities(Signature):
     name = "uses_windows_utilities"
     description = "Uses Windows utilities for basic Windows functionality"
@@ -13,54 +152,9 @@ class UsesWindowsUtilities(Signature):
     minimum = "2.0"
     references = ["http://blog.jpcert.or.jp/2016/01/windows-commands-abused-by-attackers.html"]
 
-    utilities = [
-        "at ",
-        "at.exe",
-        "attrib",
-        "chcp",
-        "del ",
-        "del.exe",
-        "dir ",
-        "dir.exe",
-        "driverquery",
-        "erase ",
-        "erase.exe",
-        "fsutil",
-        "getmac",
-        "ipconfig",
-        "nbtstat",
-        "net ",
-        "net.exe",
-        "netsh",
-        "netstat",
-        "nslookup",
-        "pathping",
-        "ping ",
-        "ping.exe",
-        "qwinsta",
-        "reg ",
-        "reg.exe",
-        "regsrv32",
-        "route",
-        "runas",
-        "rwinsta",
-        "sc ",
-        "sc.exe",
-        "schtasks",
-        "shutdown",
-        "sigverif",
-        "systeminfo",
-        "tasklist",
-        "taskkill",
-        "telnet",
-        "whoami",
-        "wmic",
-        "wusa"
-    ]
-
     def on_complete(self):
         for cmdline in self.get_command_lines():
-            for utility in self.utilities:
+            for utility in utilities:
                 if utility in cmdline.lower():
                     self.mark_ioc("cmdline", cmdline)
 
@@ -74,47 +168,26 @@ class SuspiciousCommandTools(Signature):
     authors = ["Kevin Ross"]
     minimum = "2.0"
 
-    utilities = [
-        "accesschk",
-        "accessenum",
-        "adexplorer",
-        "adinsight",
-        "adrestore",
-        "autologon",
-        "autoruns",
-        "bitsadmin",
-        "bginfo",
-        "cacls",
-        "csvde",
-        "dsquery",
-        "icacls",
-        "nltest",
-        "psexec",        
-        "psfile",
-        "psgetsid",
-        "psinfo",
-        "psping",
-        "pskill",
-        "pslist",
-        "psloggedon",
-        "psloglist",
-        "pspasswd",
-        "psservice",
-        "psshutdown",
-        "pssuspend",
-        "rexec",
-        "sdbinst",
-        "shareenum",
-        "shellrunas",
-        "volumeid",
-        "wevtutil",
-        "whois",
-        "xcacls"
-    ]
+    def on_complete(self):
+        for cmdline in self.get_command_lines():
+            for utility in risk_utilities:
+                if utility in cmdline.lower():
+                    self.mark_ioc("cmdline", cmdline)
+
+        return self.has_marks()
+
+class SysInternalsToolsUsage(Signature):
+    name = "sysinternals_tools_usage"
+    description = "Uses Sysinternals tools in order to add additional command line functionality"
+    severity = 3
+    categories = ["commands", "lateral"]
+    authors = ["Kevin Ross"]
+    minimum = "2.0"
+    references = ["docs.microsoft.com/en-us/sysinternals/downloads/"]
 
     def on_complete(self):
         for cmdline in self.get_command_lines():
-            for utility in self.utilities:
+            for utility in sysinternals:
                 if utility in cmdline.lower():
                     self.mark_ioc("cmdline", cmdline)
 
