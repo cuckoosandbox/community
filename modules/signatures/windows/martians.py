70c91e450401ab385d4a87a1240fd7a129898a47
kevross33 <kevross33@gmail.com>
2017-09-21 23:21:15 +0100

    Cleanup/fix martians sig and add in new command martian sig
    CVE-2015-1770 exploit files spawning command shells (or use most document downloaders) MD5s ae6b65ca7cbd4ca0ba86c6278c834547 & 1adf29fb9b6c6ed972f1b9609b321314

--- modules/signatures/windows/martians.py
+++ modules/signatures/windows/martians.py
@@ -17,46 +17,44 @@ class ProcessMartian(Signature):
     authors = ["Cuckoo Technologies", "Will Metcalf", "Kevin Ross"]
     minimum = "2.0"
 
-    def __init__(self, *args, **kwargs):
-        Signature.__init__(self, *args, **kwargs)
-        self.whitelist_procs = [
-            "acrord32.exe",
-            "acrord64.exe",
-            "chrome.exe",
-            "cscript.exe",
-            "excel.exe",
-            "firefox.exe",
-            "iexplore.exe",
-            "outlook.exe",
-            "powerpnt.exe",
-            "powershell.exe",
-            "winword.exe",
-            "wordview.exe",
-            "wscript.exe"
-            "wspub.exe"
-        ]
+    whitelist_procs = [
+        "acrord32.exe",
+        "acrord64.exe",
+        "chrome.exe",
+        "cscript.exe",
+        "excel.exe",
+        "firefox.exe",
+        "iexplore.exe",
+        "outlook.exe",
+        "powerpnt.exe",
+        "powershell.exe",
+        "winword.exe",
+        "wordview.exe",
+        "wscript.exe",
+        "wspub.exe"
+    ]
 
-        self.whitelist_re = [
-            "\\\"C:\\\\\Program\\ Files(\\ \\(x86\\))?\\\\Internet\\ Explorer\\\\iexplore\\.exe\\\"\\ SCODEF:\\d+ CREDAT:\\d+",
-            "^[A-Z]\:\\Program Files(?:\s\(x86\))?\\Microsoft Office\\(?:Office1[1-5]\\)?(?:WINWORD|OUTLOOK|POWERPNT|EXCEL|WORDVIEW)\.EXE",
-            "C\\:\\\\Windows\\\\System32\\\\wscript\\.exe",
-            "C\\:\\\\Program Files(?:\s\\(x86\\))?\\\\Adobe\\\\Reader\\ \\d+\\.\\d+\\\\Reader\\\\AcroRd64\\.exe",
-            "C\\:\\\\Program Files(?:\s\\(x86\\))?\\\\Adobe\\\\Reader\\ \\d+\\.\\d+\\\\Reader\\\\AcroRd64\\.exe",
-            "C\\:\\\\Program Files(?:\s\\(x86\\))?\\\\Java\\\\jre\\d+\\\\bin\\\\j(?:avaw?|p2launcher)\\.exe",
-            "C\\:\\\\Program Files(?:\s\\(x86\\))?\\\\Microsoft SilverLight\\\\(?:\\d+\\.)+\\d\\\\agcp\\.exe",
-            "C\\:\\\\Windows\\\\System32\\\\ntvdm\\.exe",
-            "C\\:\\\\Windows\\\\System32\\\\svchost\\.exe",
-            "C\\:\\\\Program Files(?:\s\\(x86\\))?\\\\internet explorer\\\\iexplore\.exe",
-            # remove this one at some point
-            "C\\:\\\\Windows\\\\System32\\\\rundll32\\.exe",
-            "C\\:\\\\Windows\\\\System32\\\\drwtsn32\\.exe",
-            "C\\:\\\\Windows\\\\splwow64\\.exe",
-            "C\\:\\\\Program Files(?:\s\\(x86\\))?\\\\Common Files\\\\Microsoft Shared\\\\office1[1-6]\\\\off(?:lb|diag)\\.exe",
-            "C\\:\\\\Program Files(?:\s\\(x86\\))?\\\\Common Files\\\\Microsoft Shared\\\\dw\\\\dw(?:20)?\\.exe",
-            "C\\:\\\\Windows\\\\system32\\\\dwwin\\.exe",
-            "C\\:\\\\Windows\\\\system32\\\\WerFault\\.exe",
-            "C\\:\\\\Windows\\\\syswow64\\\\WerFault\\.exe"
-        ]
+    whitelist_re = [
+        "\\\"C:\\\\\Program\\ Files(\\ \\(x86\\))?\\\\Internet\\ Explorer\\\\iexplore\\.exe\\\"\\ SCODEF:\\d+ CREDAT:\\d+",
+        "^[A-Z]\:\\Program Files(?:\s\(x86\))?\\Microsoft Office\\(?:Office1[1-5]\\)?(?:WINWORD|OUTLOOK|POWERPNT|EXCEL|WORDVIEW)\.EXE",
+        "C\\:\\\\Windows\\\\System32\\\\wscript\\.exe",
+        "C\\:\\\\Program Files(?:\s\\(x86\\))?\\\\Adobe\\\\Reader\\ \\d+\\.\\d+\\\\Reader\\\\AcroRd64\\.exe",
+        "C\\:\\\\Program Files(?:\s\\(x86\\))?\\\\Adobe\\\\Reader\\ \\d+\\.\\d+\\\\Reader\\\\AcroRd64\\.exe",
+        "C\\:\\\\Program Files(?:\s\\(x86\\))?\\\\Java\\\\jre\\d+\\\\bin\\\\j(?:avaw?|p2launcher)\\.exe",
+        "C\\:\\\\Program Files(?:\s\\(x86\\))?\\\\Microsoft SilverLight\\\\(?:\\d+\\.)+\\d\\\\agcp\\.exe",
+        "C\\:\\\\Windows\\\\System32\\\\ntvdm\\.exe",
+        "C\\:\\\\Windows\\\\System32\\\\svchost\\.exe",
+        "C\\:\\\\Program Files(?:\s\\(x86\\))?\\\\internet explorer\\\\iexplore\.exe",
+        # remove this one at some point
+        "C\\:\\\\Windows\\\\System32\\\\rundll32\\.exe",
+        "C\\:\\\\Windows\\\\System32\\\\drwtsn32\\.exe",
+        "C\\:\\\\Windows\\\\splwow64\\.exe",
+        "C\\:\\\\Program Files(?:\s\\(x86\\))?\\\\Common Files\\\\Microsoft Shared\\\\office1[1-6]\\\\off(?:lb|diag)\\.exe",
+        "C\\:\\\\Program Files(?:\s\\(x86\\))?\\\\Common Files\\\\Microsoft Shared\\\\dw\\\\dw(?:20)?\\.exe",
+        "C\\:\\\\Windows\\\\system32\\\\dwwin\\.exe",
+        "C\\:\\\\Windows\\\\system32\\\\WerFault\\.exe",
+        "C\\:\\\\Windows\\\\syswow64\\\\WerFault\\.exe"
+    ]
 
     def on_complete(self):
         for process in self.get_results("behavior", {}).get("generic", []):
@@ -74,5 +72,55 @@ class ProcessMartian(Signature):
                             parent_process=pname,
                             martian_process=cmdline,
                         )
+
+        return self.has_marks()
+
+class MartianCommandProcess(Signature):
+    name = "martian_command_process"
+    description = "A command shell or script process was run by an unexpected parent process"
+    severity = 3
+    categories = ["martian", "exploit", "dropper"]
+    authors = ["Cuckoo Technologies", "Will Metcalf", "Kevin Ross"]
+    minimum = "2.0"
+
+    whitelist_procs = [
+        "acrord32.exe",
+        "acrord64.exe",
+        "chrome.exe",
+        "cscript.exe",
+        "excel.exe",
+        "firefox.exe",
+        "iexplore.exe",
+        "outlook.exe",
+        "powerpnt.exe",
+        "powershell.exe",
+        "winword.exe",
+        "wordview.exe",
+        "wscript.exe"
+        "wspub.exe"
+    ]
+
+    cmd_procs = [
+        "cmd.exe",
+        "cmd ",
+        "cscript",
+        "powershell",
+        "wscript"
+    ]
+
+    def on_complete(self):
+        for process in self.get_results("behavior", {}).get("generic", []):
+            if process["process_name"].lower() not in self.whitelist_procs:
+                continue
+
+            for cmdline in process.get("summary", {}).get("command_line", []):
+               pname = process["process_name"].lower()
+               if cmdline != "":
+                   for cmd in self.cmd_procs:
+                       if cmdline.startswith(cmd):
+                           self.mark(
+                               parent_process=pname,
+                               martian_process=cmdline,
+                           )
 
         return self.has_marks()
