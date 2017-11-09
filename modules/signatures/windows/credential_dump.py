f3763bbcd68386979d846e18e553fd1de9080afc
kevross33 <kevross33@gmail.com>
2017-11-09 09:21:30 +0000

    Update credential_dump.py

--- modules/signatures/windows/credential_dump.py
+++ modules/signatures/windows/credential_dump.py
@@ -13,8 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from sets import Set
-
 from lib.cuckoo.common.abstracts import Signature
 
 class CredentialDumpingLsass(Signature):
