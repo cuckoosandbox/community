edc2798383ff5111271069c6416a1cc5815eefa7
kevross33 <kevross33@gmail.com>
2018-01-03 12:36:16 +0000

    Add in signatures for suspicious command lines (cmd, powershell etc.)

--- modules/signatures/windows/windows_utilities.py
+++ modules/signatures/windows/windows_utilities.py
@@ -2,6 +2,9 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
+# This requires python entropy package to be installed with "pip install entropy"
+import entropy
+
 from lib.cuckoo.common.abstracts import Signature
 
 class UsesWindowsUtilities(Signature):
@@ -104,5 +107,56 @@ class SuspiciousCommandTools(Signature):
             for utility in self.utilities:
                 if cmdline.lower().startswith(utility):
                     self.mark_ioc("cmdline", cmdline)
+
+        return self.has_marks()
+
+class LongCommandLine(Signature):
+    name = "long_command_line"
+    description = "A suspiciously long command line or script command was executed"
+    severity = 2
+    categories = ["commands"]
+    authors = ["Kevin Ross"]
+    minimum = "2.0"
+
+    utilities = [
+        "cmd",
+        "cscript",
+        "hta",
+        "powershell",
+        "wscript",
+    ]
+
+    def on_complete(self):
+        for cmdline in self.get_command_lines():
+            for utility in self.utilities:
+                if cmdline.lower().startswith(utility) and len(cmdline) > 150:
+                    self.mark_ioc("cmdline", cmdline)
+
+        return self.has_marks()
+
+class CommandObfuscation(Signature):
+    name = "command_obfuscation"
+    description = "A command line or script command was executed containing high entropy indicative of obfuscation"
+    severity = 3
+    categories = ["commands"]
+    authors = ["Kevin Ross"]
+    minimum = "2.0"
+
+    utilities = [
+        "cmd.exe",
+        "cmd ",
+        "cscript",
+        "hta.exe",
+        "hta ",
+        "powershell",
+        "wscript",
+    ]
+
+    def on_complete(self):
+        for cmdline in self.get_command_lines():
+            for utility in self.utilities:
+                if cmdline.lower().startswith(utility):
+                    if entropy.shannon_entropy(cmdline) > 0.56:
+                        self.mark_ioc("cmdline", cmdline)
 
         return self.has_marks()
