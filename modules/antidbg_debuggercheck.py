90187fd878d9661a8643f4d1b1f1d519f1893e35
kevross33 <kevross33@gmail.com>
2018-01-25 15:17:11 +0000

    Add in signature for debugger detection

---
+++ modules/antidbg_debuggercheck.py
@@ -0,0 +1,54 @@
+# Copyright (C) 2018 Kevin Ross
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from lib.cuckoo.common.abstracts import Signature
+
+
+class ChecksDebugger(Signature):
+    name = "checks_debugger"
+    description = "Checks if process is being debugged by a debugger"
+    severity = 1
+    categories = ["anti-debug"]
+    authors = ["Kevin Ross"]
+    minimum = "2.0"
+
+    filter_apinames = [
+        "CheckRemoteDebuggerPresent",
+        "IsDebuggerPresent",
+    ]
+
+    def on_call(self, call, process):
+        self.mark_call()
+
+    def on_complete(self):
+        return self.has_marks()
+
+class ChecksKernelDebugger(Signature):
+    name = "checks_kernel_debugger"
+    description = "Checks if process is being debugged by a kernel debugger"
+    severity = 2
+    categories = ["anti-debug"]
+    authors = ["Kevin Ross"]
+    minimum = "2.0"
+
+    filter_apinames = [
+        "SystemKernelDebuggerInformation",
+    ]
+
+    def on_call(self, call, process):
+        self.mark_call()
+
+    def on_complete(self):
+        return self.has_marks()
