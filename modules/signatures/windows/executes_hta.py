1857f6982bb598de418509bfe9036e31edcd664a
kevross33 <kevross33@gmail.com>
2018-02-07 14:09:55 +0000

    Update HTA signature to reflect current uses

--- modules/signatures/windows/executes_hta.py
+++ modules/signatures/windows/executes_hta.py
@@ -15,11 +15,11 @@
 
 from lib.cuckoo.common.abstracts import Signature
 
-class DisplaysHTA(Signature):
-    name = "displays_hta"
-    description = "Displays a HTA file to the user common in ransomware"
+class ExecutesHTA(Signature):
+    name = "executes_hta"
+    description = "Executes or displays a HTA file"
     severity = 2
-    categories = ["ransomware"]
+    categories = ["ransomware", "downloader", "exploit"]
     authors = ["Kevin Ross"]
     minimum = "2.0"
 
@@ -28,6 +28,23 @@ class DisplaysHTA(Signature):
             lower = cmdline.lower()
 
             if "mshta" in lower:
+                self.mark(cmdline=cmdline)
+
+        return self.has_marks()
+
+class ExecutesHTAJavaScript(Signature):
+    name = "executes_hta_javascript"
+    description = "Executes JavaScript using mshta utility"
+    severity = 3
+    categories = ["ransomware", "downloader", "exploit"]
+    authors = ["Kevin Ross"]
+    minimum = "2.0"
+
+    def on_complete(self):
+        for cmdline in self.get_command_lines():
+            lower = cmdline.lower()
+
+            if "mshta" in lower and "javascript:" in lower:
                 self.mark(cmdline=cmdline)
 
         return self.has_marks()
