e08deb3821d5cd5fedda6c5f9fcad0453e48c977
Alwin Peppels <alwin@cuckoo.sh>
2018-04-05 17:40:36 +0200

    signature for extracting adresses from .url files

---
+++ modules/signatures/windows/url_file.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2010-2018 Cuckoo Foundation.
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
+from lib.cuckoo.common.abstracts import Signature
+
+class URLFile(Signature):
+    name = "url_file"
+    description = "URLs have been extracted from an Internet shortcut file"
+    authors = ["Cuckoo Technologies"]
+    severity = 2
+    categories = ["generic"]
+    minimum = "2.0"
+
+
+    def __init__(self, *args, **kwargs):
+        Signature.__init__(self, *args, **kwargs)
+        if self.get_results("target", {}).get("category") == "file":
+            self.file = self.get_results("target", {}).get("file", {})
+            if "Internet shortcut" not in self.file.get("type", ""):
+                self.ignore = True
+
+    def on_complete(self):
+        if "urls" in self.file:
+            urls = self.file.get("urls", [])
+            for url in urls:
+                self.mark_ioc("extracted URL", url)
+            return True
