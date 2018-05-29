d1ca238f49ae4b35b8b86fc940bd42eb852bdac5
Jurriaan Bremer <jbr@cuckoo.sh>
2018-05-29 11:06:22 +0200

    code cleanup

--- modules/signatures/windows/excel_datalink_files.py
+++ modules/signatures/windows/excel_datalink_files.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2010-2018 Cuckoo Foundation.
+# Copyright (C) 2018 Cuckoo Foundation.
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
@@ -8,24 +8,25 @@ from lib.cuckoo.common.abstracts import Signature
 
 class ExcelDataLinks(Signature):
     name = "excel_datalink"
-    description = "External resource URLs have been extracted from an Excel" \
-                  " helper file"
+    description = (
+        "External resource URLs have been extracted from an Excel helper file"
+    )
     authors = ["Cuckoo Technologies"]
     severity = 2
     categories = ["generic"]
     minimum = "2.0"
 
-    extensions = (".iqy", ".slk")
+    extensions = ".iqy", ".slk"
 
     def on_complete(self):
         target = self.get_results("target", {})
         if target.get("category") != "file":
-            return False
+            return
 
         f = target.get("file", {})
         name, ext = os.path.splitext(f.get("name", ""))
         if ext not in self.extensions:
-            return False
+            return
 
         for url in f.get("urls", []):
             self.mark_ioc("Extracted URL", url)
