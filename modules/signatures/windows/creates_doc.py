fe6e407c7819d0047ca7eea3f96e00e12c15abd9
acd62081 <acd62081@yahoo.com>
2019-08-10 23:28:01 -0400

    Whitelisting the Microsoft Temporary Owner Files
    Microsoft states that:
    "Owner File (Same Directory as Source File)
    When a previously saved file is opened for editing, for printing, or for review, Word creates a temporary file that has a .doc file name extension. This file name extension begins with a tilde (~) that is followed by a dollar sign ($) that is followed by the remainder of the original file name."
    https://support.microsoft.com/en-us/help/211632/description-of-how-word-creates-temporary-files

--- modules/signatures/windows/creates_doc.py
+++ modules/signatures/windows/creates_doc.py
@@ -2,6 +2,9 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
+import ntpath
+import logging
+
 from lib.cuckoo.common.abstracts import Signature
 
 class CreatesDocument(Signature):
@@ -15,7 +18,18 @@ class CreatesDocument(Signature):
     pattern = ".*\\.(doc|docm|dotm|docx|ppt|pptm|pptx|potm|ppam|ppsm|xls|xlsm|xlsx|pdf)$"
 
     def on_complete(self):
+        log = logging.getLogger(__name__)
+        for fileopened in self.check_file(pattern=self.pattern, actions=["file_opened"], regex=True, all=True):
+            opened_dirpath, opened_files = ntpath.split(fileopened)
         for filepath in self.check_file(pattern=self.pattern, actions=["file_written"], regex=True, all=True):
-            self.mark_ioc("file", filepath)
-
+            file_dirpath, filepath_files = ntpath.split(filepath)
+            if opened_dirpath == file_dirpath and filepath_files[2:] in opened_files and filepath_files[0:2] == "~$":
+                if opened_dirpath == file_dirpath:
+                    log.debug("Parameter 1 of 3: {} is equal to {}...Passed...".format(opened_dirpath, file_dirpath))
+                    if filepath_files[2:] in opened_files:
+                        log.debug("Parameter 2 of 3: {} is in {}...Passed...".format(filepath_files[2:], opened_files))
+                        if filepath_files[0:2] == "~$":
+                            log.debug("Parameter 3 of 3: {} is equal to ~$...Passed...Whitelisted...".format(filepath_files[0:2])) 
+            else:
+                self.mark_ioc("file", filepath)
         return self.has_marks()
